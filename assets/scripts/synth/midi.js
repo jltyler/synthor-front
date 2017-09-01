0acce92955ad9a5b2b587f01a7d0169b950c3736
jltyler <john.lucas.tyler@gmail.com>
2017-09-01 07:18:04 -0400

    Will only attempt to connect to MIDI if it exists

--- assets/scripts/synth/midi.js
+++ assets/scripts/synth/midi.js
@@ -2,30 +2,39 @@ const SimpleMidiInput = require('simple-midi-input')
 const synth = require('./synth')
 const smi = new SimpleMidiInput()
 
-navigator.requestMIDIAccess()
-  .then(midi => {
-    smi.attach(midi)
+// console.log('typeof navigator.requestMIDIAccess');
+// console.log(typeof navigator.requestMIDIAccess);
+
+if (typeof navigator.requestMIDIAccess === 'function') {
+  // console.log('supports MIDI');
+  navigator.requestMIDIAccess()
+    .then(midi => {
+      smi.attach(midi)
+      // console.log('attached');
+    })
+    .then(() => {
+      smi.on('noteOn', data => {
+        // console.log('noteOn')
+        // console.log(data)
+        synth.playNote(data.key)
+      })
+
+      smi.on('noteOff', data => {
+        // console.log('noteOff')
+        // console.log(data)
+        synth.stopNote(data.key)
+      })
+    })
+    .catch(err => {
+      // console.log('errir!');
+      // console.log(err)
   })
-  .catch(err => {
-    // console.log(err)
-  })
+}
 
 const logMidiInputs = () => {
   // console.log('easymidi.getInputs()')
   // console.log(easymidi.getInputs())
 }
-
-smi.on('noteOn', data => {
-  // console.log('noteOn')
-  // console.log(data)
-  synth.playNote(data.key)
-})
-
-smi.on('noteOff', data => {
-  // console.log('noteOff')
-  // console.log(data)
-  synth.stopNote(data.key)
-})
 
 module.exports = {
   logMidiInputs
